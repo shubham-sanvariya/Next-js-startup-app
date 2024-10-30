0cf5fd6bfb3fd7f45d2a349f6099471639c4e7ad
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-30 23:05:50 +0530

    adding Navbar in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 import {ReactNode} from "react";
+import Navbar from "@/app/components/Navbar";
 
 export default function Layout( { children } : Readonly<{ children : ReactNode }>){
     return (
       <main className={"font-work-sans"}>
+          <Navbar />
           {children}
       </main>
     );
