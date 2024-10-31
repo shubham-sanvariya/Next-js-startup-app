525eff2f923d33c42230d280ae6cb152571866a0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-31 15:56:12 +0530

    adding section element

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,17 @@
-
-
 export default function Home() {
-  return (
-    <>
-      {/*  from global css*/}
-      <h1 className="heading">Home</h1>
-    </>
-  );
+    return (
+        <>
+            <section className="pink_container">
+                {/*  from global css*/}
+                <h1 className="heading">Pitch Your Startup,<br/>  Connect With Entrepreneurs</h1>
+
+                {/* ! is to override some other styles prev provided to this element*/}
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+                </p>
+
+            </section>
+
+        </>
+    );
 }
