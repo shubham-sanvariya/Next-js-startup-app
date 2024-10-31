f6167fd02b3c8c6463b086435f78ef6a3c345e5e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-31 15:06:56 +0530

    using heading class in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className={"text-2xl"}>Home</h1>
+      {/*  from global css*/}
+      <h1 className="heading">Home</h1>
     </>
   );
 }
