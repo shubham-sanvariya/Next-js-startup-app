cf7774a1cdac08ebcb3754a46bc7f1dcf7bdf3ad
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-31 21:18:45 +0530

    moved component directory to root

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import {ReactNode} from "react";
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/components/Navbar";
+
 
 export default function Layout( { children } : Readonly<{ children : ReactNode }>){
     return (
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Form from "next/form";
-import SearchFormReset from "@/app/components/SearchFormReset";
+import SearchFormReset from "@/components/SearchFormReset";
 import {Search} from "lucide-react";
 
 const SearchForm = ({ query } : { query?: string }) => {
 
     return (
-        <Form action="/" scroll={false} className="search-form">
+        <Form action="/public" scroll={false} className="search-form">
             <input
             name={"query"}
             defaultValue={query}
