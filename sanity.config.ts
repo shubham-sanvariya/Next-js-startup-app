035118e290586d05309ab0d0b2f35daf346cf32f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-01 16:45:09 +0530

    adding markdownSchema in sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,17 +12,19 @@ import {structureTool} from 'sanity/structure'
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
+import {markdownSchema} from "sanity-plugin-markdown";
 
 export default defineConfig({
-  basePath: '/studio',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
-  plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
+    basePath: '/studio',
+    projectId,
+    dataset,
+    // Add and edit the content schema in the './sanity/schemaTypes' folder
+    schema,
+    plugins: [
+        structureTool({structure}),
+        // Vision is for querying with GROQ from inside the Studio
+        // https://www.sanity.io/docs/the-vision-plugin
+        visionTool({defaultApiVersion: apiVersion}),
+        markdownSchema()
+    ],
 })
