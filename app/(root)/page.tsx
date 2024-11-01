2ef4cb21fb3f0b9bcee6ae9d284ff6c78fbe7a2b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-01 22:00:25 +0530

    adding StartUpCard & mapping it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,17 @@
-import SearchForm from "@/app/components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartUpCard, {StartUpTypeCard} from "@/components/StartUpCard";
+import {client} from "@/sanity/lib/client";
+import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 
 export default async function Home( {searchParams } : {
     searchParams: Promise<{ query?: string}>
 }) {
 
     const query = (await searchParams).query;
+
+    const posts = await client.fetch(STARTUPS_QUERY);
+
+
 
     return (
         <>
@@ -20,6 +27,21 @@ export default async function Home( {searchParams } : {
                 <SearchForm query={query}/>
             </section>
 
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : 'All Startups'}
+                </p>
+
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post : StartUpTypeCard) => (
+                            <StartUpCard key={post?._id} post={post}/>
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
+            </section>
         </>
     );
 }
