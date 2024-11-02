406f4b3d05c39fc7d7dc90c37ec7f0d33d8fa58d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-02 17:24:29 +0530

    adding STARTUP_BY_ID_QUERY

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -16,3 +16,18 @@ export const STARTUPS_QUERY =
     category,
     image,
            }`);
+
+export const STARTUP_BY_ID_QUERY =
+    defineQuery(`*[_type == "startup" && _id == $id][0]
+    {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {_id , username, image, bio},
+    views,
+    description,
+    category,
+    image,
+    pitch,
+           }`);
