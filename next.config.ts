96830e69bae1d46e90bf270116ff1b435f6051a6
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-02 17:35:48 +0530

    adding config for ppr request

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
                 hostname: '*',
             }
         ]
+    },
+    experimental: {
+        ppr : 'incremental'
+    },
+    devIndicators: {
+        appIsrStatus: true,
+        buildActivity: true,
+        buildActivityPosition: "bottom-right"
     }
 };
 
