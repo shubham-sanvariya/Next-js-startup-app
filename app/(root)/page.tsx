4900118e428131d62a5cb494c70b6de1e7dc54dd
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-02 16:13:26 +0530

    adding sanity live fetch for live reloading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import SearchForm from "@/components/SearchForm";
 import StartUpCard, {StartUpTypeCard} from "@/components/StartUpCard";
-import {client} from "@/sanity/lib/client";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
 
 export default async function Home( {searchParams } : {
     searchParams: Promise<{ query?: string}>
@@ -9,9 +9,7 @@ export default async function Home( {searchParams } : {
 
     const query = (await searchParams).query;
 
-    const posts = await client.fetch(STARTUPS_QUERY);
-
-
+    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
     return (
         <>
@@ -42,6 +40,8 @@ export default async function Home( {searchParams } : {
                     )}
                 </ul>
             </section>
+
+            <SanityLive />
         </>
     );
 }
