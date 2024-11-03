5476ffd2bb3d31f1f3fb67b0429880139800a50c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-03 18:12:14 +0530

    adding startup create path page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartUpCard, {StartUpTypeCard} from "@/components/StartUpCard";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {auth} from "@/auth";
 
 export default async function Home( {searchParams } : {
     searchParams: Promise<{ query?: string}>
@@ -9,6 +10,11 @@ export default async function Home( {searchParams } : {
 
     const query = (await searchParams).query;
     const params = { search: query || null };
+
+    const session = await auth();
+    console.log(session?.id);
+
+
     const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY , params});
 
     return (
---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+       <section className={'pink_container !min-h-[230px]'}>
+           <h1 className={'heading'}>
+               Submit Your Startup
+           </h1>
+       </section>
+    )
+}
+export default Page
