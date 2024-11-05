87cbe5f84507131edbdaaa4e576f67dac0b31721
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-05 20:22:04 +0530

    adding STARTUPS_BY_AUTHOR_QUERY

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -58,4 +58,19 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
         image,
         bio
     }
-`)
+`);
+
+export const STARTUPS_BY_AUTHOR_QUERY =
+    defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc)
+    {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    views,
+    author -> {_id , name, image, bio},
+    views,
+    description,
+    category,
+    image,
+           }`);
