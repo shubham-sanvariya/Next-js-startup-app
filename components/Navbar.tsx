71ecbeb7b3c366ffbdbbd3e0560223910c6e6b9b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-11-05 22:13:46 +0530

    adding Avatar skeleton

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from "next/dist/client/legacy/image";
 import Link from "next/link";
 import {auth, signIn, signOut} from "@/auth";
+import {BadgePlus, LogOut} from "lucide-react";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 const Navbar = async () => {
 
@@ -19,7 +21,8 @@ const Navbar = async () => {
                     {session && session?.user ? (
                         <>
                             <Link href="/startup/create">
-                                <span>Create</span>
+                                <span className={'max-sm:hidden'}>Create</span>
+                                <BadgePlus className="size-6 sm:hidden"/>
                             </Link>
 
                             <form action={async () => {
@@ -27,11 +30,20 @@ const Navbar = async () => {
 
                                 await signOut({redirectTo: "/"})
                             }}>
-                                <button type="submit">Logout</button>
+                                <button type="submit">
+                                    <span className={'max-sm:hidden'}>Logout</span>
+                                    <LogOut className={'size-6 sm:hidden text-red-500'}/>
+                                </button>
                             </form>
 
                             <Link href={`/user/${session?.id}`}>
-                                <span>{session?.user?.name}</span>
+                                <Avatar className={'size-10'}>
+                                    <AvatarImage src={session?.user?.image || ''}
+                                                 alt={session?.user?.name}/>
+                                    <AvatarFallback>
+                                        AV
+                                    </AvatarFallback>
+                                </Avatar>
                             </Link>
                         </>
                     ) : (
